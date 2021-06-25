962d0b73f23e5576bf3dfdc6c0ccdd1aab2f04e4
TheCell <simon_hischier@hotmail.com>
2021-06-25 12:17:12 +0200

    unit selection and commanding working

---
+++ RealTimeStrategy/Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+    [SerializeField] private UnitMovement unitMovement = null;
+
+    public UnitMovement GetUnitMovement()
+    {
+        return unitMovement;
+    }
+
+    #region Client
+    public void Select()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
--- RealTimeStrategy/Assets/Scripts/Units/UnitMovement.cs
+++ RealTimeStrategy/Assets/Scripts/Units/UnitMovement.cs
@@ -1,7 +1,6 @@
 using Mirror;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.InputSystem;
 
 public class UnitMovement : NetworkBehaviour
 {
@@ -13,7 +12,7 @@ public class UnitMovement : NetworkBehaviour
     #region Server
 
     [Command]
-    private void CmdMove(Vector3 position)
+    public void CmdMove(Vector3 position)
     {
         if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas))
         {
@@ -21,38 +20,6 @@ public class UnitMovement : NetworkBehaviour
         }
 
         agent.SetDestination(hit.position);
-    }
-
-    #endregion
-
-    #region Client
-
-    public override void OnStartAuthority()
-    {
-        mainCamera = Camera.main;
-        //base.OnStartAuthority();
-    }
-
-    [ClientCallback]
-    private void Update()
-    {
-        if (!hasAuthority)
-        {
-            return;
-        }
-
-        if (!Mouse.current.rightButton.wasPressedThisFrame)
-        {
-            return;
-        }
-
-        var ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
-        {
-            return;
-        }
-
-        CmdMove(hit.point);
     }
 
     #endregion
