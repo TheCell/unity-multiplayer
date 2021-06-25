0010faee588a863d084da5176d2980e3e86b5741
TheCell <simon_hischier@hotmail.com>
2021-06-25 11:21:10 +0200

    spawner and network spawning

--- RealTimeStrategy/Assets/Scripts/Networking/RTSNetworkManager.cs
+++ RealTimeStrategy/Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,4 +5,18 @@ using Mirror;
 
 public class RTSNetworkManager : NetworkManager
 {
+    [SerializeField]
+    private GameObject unitSpawnerPrefab = null;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(
+            unitSpawnerPrefab, 
+            conn.identity.transform.position, 
+            conn.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
