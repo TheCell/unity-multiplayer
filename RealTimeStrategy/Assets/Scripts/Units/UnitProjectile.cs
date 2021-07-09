6804930d00c90be839c0ba734c146ca6d55b6fc0
TheCell <simon_hischier@hotmail.com>
2021-07-09 13:12:49 +0200

    added health logic and object destroying

--- RealTimeStrategy/Assets/Scripts/Units/UnitProjectile.cs
+++ RealTimeStrategy/Assets/Scripts/Units/UnitProjectile.cs
@@ -6,6 +6,7 @@ using Mirror;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -17,6 +18,26 @@ public class UnitProjectile : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient)
+            {
+                // we hit our own unit
+                return;
+            }
+
+            if (other.TryGetComponent<Health>(out Health health))
+            {
+                health.DealDamage(damageToDeal);
+            }
+
+            DestroySelf();
+        }
     }
 
     [Server]
