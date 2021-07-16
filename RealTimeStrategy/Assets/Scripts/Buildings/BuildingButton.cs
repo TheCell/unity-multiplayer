601f54a3e8663a02b02aad1968544b09626366cc
TheCell <simon_hischier@hotmail.com>
2021-07-16 20:01:13 +0200

    spawn building part 1

---
+++ RealTimeStrategy/Assets/Scripts/Buildings/BuildingButton.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class BuildingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Building building = null;
+    [SerializeField] private Image iconImage = null;
+    [SerializeField] private TMP_Text priceText = null;
+    [SerializeField] private LayerMask floorMask = new LayerMask();
+
+    private Camera mainCamera;
+    private RTSPlayer player;
+    private GameObject buildingPreviewInstance;
+    private Renderer buildingRendererInstance;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        iconImage.sprite = building.GetIcon();
+        priceText.text = building.GetPrice().ToString();
+
+        if (buildingPreviewInstance == null)
+        {
+            return;
+        }
+
+        UpdateBuildingPreview();
+    }
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        }
+
+        if (buildingPreviewInstance == null)
+        {
+            return;
+        }
+
+        UpdateBuildingPreview();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+        {
+            return;
+        }
+
+        Debug.Log(eventData.button);
+        buildingPreviewInstance = Instantiate(building.GetBuildingPreview());
+        buildingRendererInstance = buildingPreviewInstance.GetComponentInChildren<Renderer>();
+
+        buildingPreviewInstance.SetActive(false);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (buildingPreviewInstance == null)
+        {
+            return;
+        }
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask))
+        {
+            // place building
+        }
+
+        Destroy(buildingPreviewInstance);
+    }
+
+    private void UpdateBuildingPreview()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask))
+        {
+            return;
+        }
+
+        buildingPreviewInstance.transform.position = hit.point;
+
+        if (!buildingPreviewInstance.activeSelf)
+        {
+            buildingPreviewInstance.SetActive(true);
+        }
+    }
+}
