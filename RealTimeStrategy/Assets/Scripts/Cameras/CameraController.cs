7c80ed072857739d19bbb233be61f1ccc0766cea
TheCell <simon_hischier@hotmail.com>
2021-07-30 11:59:39 +0200

    minimap camera move

--- RealTimeStrategy/Assets/Scripts/Cameras/CameraController.cs
+++ RealTimeStrategy/Assets/Scripts/Cameras/CameraController.cs
@@ -77,7 +77,6 @@ public class CameraController : NetworkBehaviour
         pos.x = Mathf.Clamp(pos.x, screenXLimits.x, screenXLimits.y);
         pos.z = Mathf.Clamp(pos.z, screenZLimits.x, screenZLimits.y);
 
-        Debug.Log(pos);
         playerCameraTransform.position = pos;
     }
 
