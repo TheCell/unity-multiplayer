91ee07131e689a46d629b90552ac7d85e8584834
TheCell <simon_hischier@hotmail.com>
2021-08-27 09:39:09 +0200

    added lobby menu

---
+++ RealTimeStrategy/Assets/Scripts/Menus/LobbyMenu.cs
@@ -0,0 +1,40 @@
+using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject lobbyUi = null;
+
+    private void Start()
+    {
+        RTSNetworkManager.ClientOnConnected += HandleClientConnected;
+    }
+
+    private void OnDestroy()
+    {
+        RTSNetworkManager.ClientOnConnected -= HandleClientConnected;
+    }
+
+    private void HandleClientConnected()
+    {
+        lobbyUi.SetActive(true);
+    }
+
+    public void LeaveLobby()
+    {
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            NetworkManager.singleton.StopHost();
+        }
+        else
+        {
+            NetworkManager.singleton.StopClient();
+
+            SceneManager.LoadScene(0);
+        }
+    }
+}
